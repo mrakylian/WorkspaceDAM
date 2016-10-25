3b16c5c2f3837dcc010d2955ff1d1fdc05aea967
alumnodiurno <alumnodiurno@PC15>
2016-10-25 10:08:40 +0200

    DAM

--- Relacion1/src/Ejercicio3Relacion3.java
+++ Relacion1/src/Ejercicio3Relacion3.java
@@ -15,19 +15,39 @@ public class Ejercicio3Relacion3 {
 		
 		//VARIABLES
 		
+		//Declaramos la variables
+		
 		int numero, totalCifras, numeroActual;
 		
 		//INICIO
+		
+		//Solicitamos por pantalla el numero
+		
 		System.out.println("Introduce un numero entero");
+		
+		//Leemos el numero
+		
 		numero = Integer.parseInt(teclado.nextLine());
+		
+		//Declaramos las variables
 		
 		numeroActual = numero;
 		totalCifras = 1;
 		
+		//Mientras numeroActual entre 10 sea distinto de 0
+		
 		while ( numeroActual / 10 != 0) {
+			
+		//Si numeroActual es igual el numero actual entre 10
+			
 			numeroActual = numeroActual / 10;
+			
+		//Se incrementa el contador en 1	
+			
 			totalCifras ++;
 		}
+		
+		//Se muestra el resultado
 		
 		System.out.println("El numero " + numero + " tiene " + totalCifras + " cifras.");
 	}
---
+++ Relacion1/src/Ejercicio4Relacion3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+/**
+ * Realizar un programa que solicite dos números enteros y muestre por
+pantalla el máximo común divisor de los dos números.
+ * @author alumnodiurno
+ *
+ */
+public class Ejercicio4Relacion3 {
+	private static Scanner teclado = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		//VARIABLES
+		
+		int numero1, numero2, maximoComunDivisor;
+		
+		//INICIO
+		
+		while (numero / 2 = )
+		
+
+	}
+
+}
