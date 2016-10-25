bd7a85e50c48af491a6ad1085e78400e57f6367d
alumnodiurno <alumnodiurno@PC15>
2016-10-25 09:46:46 +0200

    DAM

--- Relacion1/src/Ejercicio3Relacion3.java
+++ Relacion1/src/Ejercicio3Relacion3.java
@@ -1,9 +1,35 @@
-
+import java.util.Scanner;
+/**
+ * Realizar un programa que solicite un número entero y determine el número
+de cifras que tiene dicho número. 
+Ejemplo:
+Introduce un número: 124
+El número 124 tiene 3 cifras
+ * @author alumnodiurno
+ *
+ */
 public class Ejercicio3Relacion3 {
-
+	
+	private static Scanner teclado = new Scanner(System.in);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//VARIABLES
+		
+		int numero, totalCifras, numeroActual;
+		
+		//INICIO
+		System.out.println("Introduce un numero entero");
+		numero = Integer.parseInt(teclado.nextLine());
+		
+		numeroActual = numero;
+		totalCifras = 1;
+		
+		while ( numeroActual / 10 != 0) {
+			numeroActual = numeroActual / 10;
+			totalCifras ++;
+		}
+		
+		System.out.println("El numero " + numero + " tiene " + totalCifras + " cifras.");
 	}
 
 }
